07de8773b2e13c488cac89b9f2905c1a05eb1f4c
Timo Schuermann <timo.schuermann91@gmx.de>
2019-11-25 10:43:40 +0100

    s

--- R/add_IpNumber.R
+++ R/add_IpNumber.R
@@ -16,12 +16,17 @@ add_IpNumber <- function(events_sorted){
     if((stringr::str_ends(events_sorted$Text[k-1], "[.;?,-]")|
         stringr::str_ends(events_sorted$Text[k-1], "\\Q|\\E")|
         stringr::str_ends(events_sorted$Text[k-1], "\\u01C0") |
+        stringr::str_ends(events_sorted$Text[k-1], "\\Q/\\E"))&stringr::str_starts(events_sorted&Text[k], "=")){
+      IpNumber[k] <- IpNumber[k-1]
+      k <- k+1
+    }else if(stringr::str_ends(events_sorted$Text[k-1], "[.;?,-]")|
+        stringr::str_ends(events_sorted$Text[k-1], "\\Q|\\E")|
+        stringr::str_ends(events_sorted$Text[k-1], "\\u01C0") |
         stringr::str_ends(events_sorted$Text[k-1], "\\Q/\\E")|
         stringr::str_ends(events_sorted$Text[k-1], "([.;?,-]{1}| \\Q|\\E|\\Q/\\E|\\u01C0)(?=>)")|
         stringr::str_ends(events_sorted$Text[k-1], "\\d\\)")|
         stringr::str_ends(events_sorted$Text[k-1], "\\)\\)")|
-        stringr::str_ends(events_sorted$Text[k-1], "([.;?,-]{1}| \\Q|\\E|\\Q/\\E|\\u01C0)(?=\\])"))
-       & (stringr::str_ends(events_sorted[k-1], "[^=]")& stringr::str_starts(events_sorted$Text[k], "=") == FALSE)){
+        stringr::str_ends(events_sorted$Text[k-1], "([.;?,-]{1}| \\Q|\\E|\\Q/\\E|\\u01C0)(?=\\])")){
       IpNumber[k] <- IpNumber[k-1] +1
       k <- k+1
     } else if(events_sorted$Speaker[k-1] != events_sorted$Speaker[k]){
