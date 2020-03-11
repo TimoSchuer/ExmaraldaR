6c7acf447265baae2deb734c0ca3eda34f3a09e3
Timo Schuermann <timo.schuermann91@gmx.de>
2020-03-11 16:48:41 +0100

    Speed improvement read_timeline

--- R/read_timeline.R
+++ R/read_timeline.R
@@ -10,9 +10,7 @@
 #' @examples
 #' read_timeline(file)
 read_timeline <- function(file) {
-  timeline <- c()
-  for (n in 1:length(xml2::xml_children(xml2::xml_child(xml2::xml_child(file, 2), 1)))) {
-    timeline[n] <- xml2::xml_attrs(xml2::xml_child(xml2::xml_child(xml2::xml_child(file, 2), 1), n))[["id"]]
-  }
+    timeline <- xml2::xml_attrs(xml2::xml_children(xml2::xml_child(xml2::xml_child(file, 2), 1))) %>% do.call(rbind, .)
+    timeline <- timeline[,1]
   return(timeline)
 }
