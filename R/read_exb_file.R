c936a4feb3e05cff0aa3c5c9facb84004c411fbc
Timo Schuermann <timo.schuermann91@gmx.de>
2020-03-12 14:08:17 +0100

    Update Documentation

--- R/read_exb_file.R
+++ R/read_exb_file.R
@@ -5,6 +5,7 @@
 #' @param path Path of an exb transcription file
 #' @param addMetaData Logical Value, wheter Metadata should be read from the speakertable
 #' @param sortMetaDate Logical value, wheter metadata should be sorted directly after the speaker name or at the end
+#' @param readAnn Logical Value, whetaer annotation tiers should be read and sorted
 #'
 #' @return Returns a data frame that contains the transcription and the annotations
 #' @export
