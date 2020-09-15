b2bfece36838559ba37691b75d72b2be4fe14800
Timo Schuermann <timo.schuermann91@gmx.de>
2020-09-15 10:59:34 +0200

    commeting

--- R/sort_annotations_multilayer.R
+++ R/sort_annotations_multilayer.R
@@ -19,12 +19,12 @@ sort_annotations_multilayer <- function(file, AnnotationTiers,events_sorted){
     for (k in 2:length(AnnotationTiers)) {
       AnnText <- xml2::xml_text( xml2::xml_children(AnnotationTiers[k]))
       AnnText[AnnText==""] <- NA #replace empty events with NA
-      if(length(AnnText)>0){
+      if(length(AnnText)>0){#control for empty annotation tiers
         time <-  xml2::xml_attrs(xml2::xml_children(AnnotationTiers[[k]]))
         time <- data.frame(matrix(unlist(time), ncol = max(lengths(time)), byrow = TRUE))
         Speaker <-  xml2::xml_attr(AnnotationTiers[[k]], "speaker")
         AnnTier <- data.frame(Speaker= Speaker, Start= time[,1], End= time[,2],AnnText= AnnText)
-        AnnTier<- `colnames<-`(AnnTier, c("Speaker","Start","End",xml2::xml_attr(AnnotationTiers[k], "display-name")))
+        AnnTier<- `colnames<-`(AnnTier, c("Speaker","Start","End",xml2::xml_attr(AnnotationTiers[k], "category")))
         exb <- suppressWarnings(dplyr::left_join(exb, AnnTier, by=c("Speaker", "Start", "End")))
         MultiEventAnn <- dplyr::anti_join( AnnTier,events_sorted, by=c("Speaker", "Start", "End")) # check for annotations for more than 1 event
         if(nrow(MultiEventAnn)!=0){
