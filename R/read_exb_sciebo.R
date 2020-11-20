d9e3842fc188ba220c883622516a621f62fc7574
Timo Schuermann <timo.schuermann91@gmx.de>
2020-11-20 18:51:36 +0100

    minor changes

--- R/read_exb_sciebo.R
+++ R/read_exb_sciebo.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param file
+#' @param name
+#'
+#' @return
+#' @export
+#'
+#' @examples
 read_events_xml <- read_events <- function(file,name){
   transcriptions <- xml2::xml_find_all(file, "/basic-transcription/basic-body[1]/tier[@type='t']") # findet alle Transkriptionszeilen; da ich immer eine Pausenzeile (benannt mit P) einfüge, wird diese ausgeschlossn
   event <- character(0)
