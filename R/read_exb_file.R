d2e92a405c4261520261e2a8e4fb8aadd9931440
TimoSchuer <57065543+TimoSchuer@users.noreply.github.com>
2022-09-13 13:43:34 +0200

    Update read_exb_file.R

--- R/read_exb_file.R
+++ R/read_exb_file.R
@@ -16,11 +16,11 @@ read_exb_file <- function(path, readAnn=TRUE,addDescription= FALSE, addMetaData=
   }else{
     file <- xml2::read_xml(path, encoding="UTF-8")
     timeline <- xml2::xml_attrs(xml2::xml_children(xml2::xml_child(xml2::xml_child(file, 2), 1))) %>% dplyr::bind_rows()
-    events <- read_events(file, path) %>%  #read events
+    events <- ExmaraldaR:::read_events(file, path) %>%  #read events
       dplyr::left_join(., timeline[,1:2], by= c("Start" ="id"), suffix= c("",".y") ) %>% dplyr::rename(Start_time= time)%>% dplyr::mutate(Start_time=as.double(Start_time)) %>%  #allocate absoulute times to time stamps
       dplyr::left_join(., timeline[,1:2], by= c("End" ="id"), suffix= c("",".y") ) %>% dplyr::rename(End_time= time) %>% dplyr::mutate(End_time= as.double(End_time)) %>%
       .[,c("File","Speaker", "TierID",  "Start","End", "Start_time", "End_time","Name","Text")] # nice and tidy order
-    events <- sort_events(events, addIPNumber= addIPNumber, IPEndSign= IPEndSign)
+    events <- ExmaraldaR:::sort_events(events, addIPNumber= addIPNumber, IPEndSign= IPEndSign)
     if(readAnn==TRUE & length(xml2::xml_find_all(file,".//tier[@type='a']"))!=0){
       AnnotationTiers <- xml2::xml_find_all(file,".//tier[@type='a']") #findet alle Annotationsspuren
       annotations <- data.frame()
