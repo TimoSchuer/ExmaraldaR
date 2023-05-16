363ce9a83bcaa8f3b9f37c0faf657a5439018e0b
Timo Schuermann <timo.schuermann91@gmx.de>
2023-05-16 15:08:02 +0200

    minor fix

--- R/sort_events.R
+++ R/sort_events.R
@@ -1,6 +1,8 @@
 sort_events <- function(events, IPEndSign= c("|",".",";",",",",","?","=","-"),addIPNumber=TRUE){
   if(addIPNumber==FALSE){
-    events <- events %>% dplyr::arrange(Start_time) %>% dplyr::mutate(EventID= paste(File,seq(1:nrow(.)))) # nolint
+    events <- events %>% 
+      dplyr::arrange(Start_time) %>% 
+      dplyr::mutate(EventID= paste(File,seq(1:nrow(.)))) # nolint
     return(events)
   }
   #build regex
@@ -15,8 +17,9 @@ sort_events <- function(events, IPEndSign= c("|",".",";",",",",","?","=","-"),ad
   IPRank <- events %>% dplyr::ungroup() %>% dplyr::group_by(TierIPNo) %>% dplyr::summarise(min=min(Start_time)) %>% dplyr::ungroup() %>% dplyr::arrange(min) %>% dplyr::pull(TierIPNo)
   events <- events %>% dplyr::ungroup() %>%
     dplyr::arrange(factor(TierIPNo, levels = IPRank)) %>%
-    dplyr::mutate(IPNumber = rle(TierIPNo)$lengths %>% {rep(seq(length(.)), .)} %>% formatC(width = 5, format = "d", flag = "0")) %>%
-    dplyr::mutate(EventID= paste(File,seq(1:nrow(.)))) %>%
+    dplyr::mutate(IPNumber = rle(TierIPNo)$lengths %>% {rep(seq_along(length(.)), .)} %>% 
+    formatC(width = 5, format = "d", flag = "0")) %>%
+    dplyr::mutate(EventID= paste(File,seq_len(nrow(.)))) %>%
     dplyr::select(IPNumber,EventID, File, Speaker, TierID, Start,End, Start_time,End_time,Name, Text)
   return(events)
 
